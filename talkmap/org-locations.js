5c09a4b28a30aa94cedd00f07ad92cb417ac7332
Adriano Barreto <adrianobraga@ymail.com>
2018-06-11 20:32:10 -0300

    mapa

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,16 +1,6 @@
 var addressPoints = [
   [
-    "Mar Del Plata, Argentina",
-    -37.9977225,
-    -57.5482703
-  ],
-  [
-    "Bad Honnef, Germany",
-    50.6448582,
-    7.2272534
-  ],
-  [
-    "S\u00e3o Paulo, Brazil",
+    "S\u00e3o Paulo, Brasil",
     -23.5506507,
     -46.6333824
   ],
@@ -18,5 +8,20 @@ var addressPoints = [
     "Valpara\u00edso, Chile",
     -33.0458456,
     -71.6196749
+  ],
+  [
+    "Mar Del Plata, Argentina",
+    -37.9977225,
+    -57.5482703
+  ],
+  [
+    "Bad Honnef, Alemanha",
+    50.6448582,
+    7.2272534
+  ],
+  [
+    "Jo\u00e3o Pessoa, Brasil",
+    -7.1215981,
+    -34.882028
   ]
 ];
